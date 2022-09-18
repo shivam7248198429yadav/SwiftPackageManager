8a2cf094473c04ab48ded09263f3c5bd6dfcaab2
Shivam Yadav <shivam.yadav@rapipay.com>
2022-09-19 00:42:36 +0530

    Initial Commit

---
+++ Sources/SwiftPackageManager/SwiftPackageManager.swift
@@ -0,0 +1,6 @@
+public struct SwiftPackageManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftPackageManagerTests/SwiftPackageManagerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftPackageManager
+
+final class SwiftPackageManagerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftPackageManager().text, "Hello, World!")
+    }
+}
