87feca0630288cf8dc99bb64881529ac1bb8651c
Shivam Yadav <shivam.yadav@rapipay.com>
2022-09-19 02:27:21 +0530

    upload package manager

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftPackageManager",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "SwiftPackageManagerTests",
             dependencies: ["SwiftPackageManager"]),
